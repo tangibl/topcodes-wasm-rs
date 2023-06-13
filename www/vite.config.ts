50bcd7e698a95ef74249e5352be60321fc16adbb
Byron Batteson <byronbatteson@gmail.com>
2023-06-13 09:43:08 +0200

    Set base path for vite build
    The GitHub page base URL is prefixed with the repo name.

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,6 +3,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 const config: UserConfigExport = {
+  base: "/topcodes-wasm-rs/",
   plugins: [
     wasm(),
     topLevelAwait(),
